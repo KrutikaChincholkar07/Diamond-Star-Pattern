519fd78a292ee8bf868cb4da3891ed7db9dbf0f1
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-09-16 23:49:06 +0530

    Create DiamondPattern.c
    Diamond Star Pattern code in C
    Output:
          *
        * * *
      * * * * *
    * * * * * * *
      * * * * *
        * * *
          *

---
+++ DiamondPattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 4;
+
+    // Upper part
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            printf("  ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // Lower part
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int j = 1; j <= rows - i; j++) {
+            printf("  ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
